c32e5c437eb1d04a7d5e90f9d6870ad690a02e19
MILENA OLIVEIRA MARES <47269169802@info.local>
2026-05-20 13:16:21 -0300

    novos

--- Produto/src/components/produto/produto.jsx
+++ Produto/src/components/produto/produto.jsx
@@ -1,200 +1,274 @@
 import "./produto.css"
 import { useEffect, useState } from "react"
 import img from '../../assets/image.jpg'
+import axios from "axios";
 
 export default function Produto() {
-
-    const [nome, setNome] = useState("")
-    const [preco, setPreco] = useState(0)
-    const [descricao, setDescricao] = useState("")
-    const [quantidade, setQuantidade] = useState(0)
-    const [imagem, setImagem] = useState(img)
-    const [editar, setEditar] = useState (false)
+   //states e variaveis
+    const [id, setId] = useState(0); //usado no editar
+    const [nome, setNome] = useState("");
+    const [preco, setPreco] = useState(0);
+    const [descricao, setDescricao] = useState("");
+    const [quantidade, setQuantidade] = useState(0);
+    const [imagem, setImagem] = useState(img);
+    const [editar, setEditar] = useState(false)
     // const [produto, setProduto] = useState({ nome: "", preco: 0, descricao: "", quantidade: 0, imagem: "" })
-
     const [arrProdutos, setArrProdutos] = useState([])
 
-    async function cadastrarProduto(e) {
-        e.preventDefault()// nao deixa o formulario ser postado (nao da f5 na pagina)
-       
-       
-    //   alert("Funcao Cadastrar Chamada") 
-    //   return false
-       
-        //validar o formulario
-        if (
-            nome.trim().length == 0 || descricao.trim().length == 0 || isNaN(preco) || isNaN(quantidade)
-        ) {
-            alert("Preencha os campos corretamente!")
-            return false
-        }
-        //gerar o objeto que vai pra API
+   // ciclos de vida e funcoes
+     async function cadastrarProduto(e) {
+        e.preventDefault()//nao deixa o formulario ser postado
+
+        // alert("Funcao Cadastrar Chamada")
+        // return false
+        //validar o formulario 
+       if (
+         nome.trim().length == 0 || 
+         descricao.trim().length == 0 ||
+         isNaN(preco) || preco <= 0 ||
+         isNaN(quantidade) || quantidade <= 0
+     ) {
+
+        alert("Preencha os campos corretamente!")
+        return false;
+      }
+        //gerar o objeto que vai pra api
+        //object short sintaxe
         const objCadastro = {
             nome,
-            preco,
             descricao,
-            quantidade: quantidade,
+            preco,
+            quantidade,
             imagem: "image.jpg",
         }
+
         console.log(objCadastro);
 
-
-        //cadastrar na API
+        //cadastrar na api
         try {
-            const retornoAPI = await fetch("http://localhost:3000/produtos", {
-                method: "POST",
-                body: JSON.stringify(objCadastro),
-                headers: { "Content-Type": "application/json; charset=UTF-8" }
-            })
+            const retornoAPI = await axios.post("http://localhost:3000/produtos", objCadastro);
+            
 
-            console.log(retornoAPI);
+          console.log(retornoAPI);
+          //validando o retorno da API
+          if(retornoAPI.status == 201) {
+            const dadosCadastrados = await retornoAPI.data;
+            // console.log(dadosCadastrados);
+            setArrProdutos([...arrProdutos, dadosCadastrados])
+            alert("Produto cadastrado com sucesso")
+            limparFormulario();
+            
+          }else {
+            alert("Problema inesperado")
+          }
 
-            //validando o retorno da API
-            if (retornoAPI.status == 201) {
-                const dadosCadastrados = retornoAPI.json()
-                console.log(dadosCadastrados);//dado que acabou de ser cadastrado
-                setArrProdutos({ ...arrProdutos, dadosCadastrados })
-                alert("Produto cadastrado com sucesso!")
-
-                limparFormulario()
-
-            } else {
-                alert("Problema inesperado!")
-            }
-
-        } catch (error) {
-            alert("Nao foi possivel salvar os dados!")
-            console.log(error);
+        } catch (e) {
+            alert("Nao foi possivel salvar os dados");
+            console.log(e);
         }
 
-        // setArrProdutos([...arrProdutos, { ...produto, id: Date.now() }])
     }
 
-    //funcao que reinicia os states para limpar o formulario
     function limparFormulario() {
-        setNome("")
-        setDescricao("")
-        setQuantidade(0)
-        setPreco(0)
+        setId(0)
+        setNome("");
+        setDescricao("");
+        setQuantidade(0);
+        setPreco(0);
+             
     }
+     
+      useEffect(() => {
+        getProdutos()
+      }, []);
 
-    useEffect(() => {
-        getProdutos();
-    }, []);
+      async function getProdutos() {
+           try {
+              const retornoAPI = await axios.get("http://localhost:3000/produtos")
+              const dados = await retornoAPI.data;
 
-    async function getProdutos() {
-            try {
-                //faz requisicao na API
-                const retornoAPI = await fetch("http://localhost:3000/produtos")
-                //transforma o retorno que eh json em objeto javascript
-                const dados = await retornoAPI.json()
+              console.log(dados)
+              setArrProdutos(dados)
+           } catch (e) {
+              console.log("erro ao buscar os produtos");
+              console.log(e);
 
-                console.log(dados);
-                //inserir os dados em state
-                setArrProdutos(dados)
-
-            } catch (error) {
-                console.log("Erro ao buscar os produtos!");
-                console.log(error);
-            }
+           } 
         }
 
+
+      async function deletar(id) {
+
+        if (!confirm("Voce realemnte quer apagar o produto?")){
+            return false;
+        }
+
+        try {
+          const retornoAPI = await api.delete(`http://localhost:3000/produtos/${id}`);
+
+          if (retornoAPI.status == 200 && retornoAPI.statusText == "OK") {
+            alert("Produto apagado com sucesso")
+
+          //gerar uma nova lista com os produtos que sobraram
+          const novaLista = arrProdutos.filter((prod) => {
+            return prod.id != id;
+          });
+
+           setArrProdutos(novaLista)
+        } else {
+            alert("Algum erro ocorreu ao apagar")
+        }
+        } catch (e) {
+            alert("Erro ao apagar o produto")
+            console.log(e);
+        }
+      }
+
+
+       async function  editarProduto(e) {
+        e.preventDefault()
+        
+        if (
+         nome.trim().length == 0 || 
+         descricao.trim().length == 0 ||
+         isNaN(preco) || preco <= 0 ||
+         isNaN(quantidade) || quantidade <= 0
+     ) {
+        alert("Preencha os campos corretamente!")
+        return false;
+      }
+        //gerar o objeto que vai pra api
+        //object short sintaxe
+        const objCadastro = {
+            nome,
+            descricao,
+            preco,
+            quantidade,
+            imagem: "image.jpg",
+        };
+
+         //chamar a api
+          try {
+
+            const retornoAPI = await axios.put(`http://localhost:3000/produtos/${id}`, objCadastro);
+
+          if(retornoAPI.status == 200) {
+            alert("Produto alterado com sucesso");
+              getProdutos();
+              limparFormulario();
+              setEditar(false);
+          }else {
+            alert("Erro ao editar")
+          }
+
+          } catch (e) {
+            alert("erro ao editar o produto")
+            console.log(e);
+          }
         
 
-    async function Deletar(id) {
-        try {
-            const retornoAPI = await fetch(`http://localhost:3000/produtos/${id}`, {
-                method: "DELETE",
-            })
-
-            //gerar uma nova lista com os produtos que sobraram
-            if (retornoAPI.status == 200 && retornoAPI.statusText == "OK") {
-                alert("Produto apagado com sucesso!")
-
-                const novaLista = arrProdutos.filter((prod) => {
-                    return prod.id != id
-                })
-
-                //atualiza a variavel de produtos
-                setArrProdutos(novaLista)   
-            } else {
-                alert("Algum erro ocorreu ao apagar")
-            }
-
-        } catch (error) {
-            alert("Erro ao apagar o produto!")
-            console.log(error);
-        }
-    }
-
-    function editarProduto(e) {
-        e.preventDefault()
-        alert("Funcao Editar Chamada")
-    }
-
+       }
+     //desenho do componente na tela em si
     return (
         <>
             <header className="cabecalho">
-                <h1 className="titulo--branco" >SENAI</h1>
-                <h1 className="titulo--azul">LOJA</h1>
+            <h1 className="titulo--cinza" >SENAI</h1>
+            <h1 className="titulo--vermelho">LOJA</h1>
             </header>
 
             <form className="formzin" action="" onSubmit={editar ? editarProduto : cadastrarProduto}>
+
                 {/* <div className="input--image">
                     <input className="input--metade" type="text" id="imagem" placeholder="Image" onChange={(e) => setProduto({ ...produto, imagem: e.target.value })} />
                 </div> */}
                 <div className="input--dados">
 
-                    <input className="input--metade" type="text" id="nome" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-                    <input className="input--metade" type="number" id="preco" placeholder="Preço" value={preco} onChange={(e) => setPreco(parseFloat(e.target.value))} />
-                    <input className="input--metade" type="number" id="quantidade" placeholder="Quantidade" value={quantidade} onChange={(e) => setQuantidade(parseInt(e.target.value))} />
-                    <input className="input--metade" type="text" id="descricao" placeholder="Descrição" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+                <input 
+                className="input--metade"
+                 type="text" 
+                 id="nome" 
+                 placeholder="Nome" 
+                 value={nome} 
+                 onChange={(e) => setNome( e.target.value) } />
+
+                <input 
+                className="input--metade" 
+                type="number" 
+                id="preco" 
+                placeholder="Preço"
+                 value={preco}  
+                 onChange={(e) => setPreco(parseFloat( e.target.value))} />
+
+                <input 
+                className="input--metade" 
+                type="number" 
+                id="quantidade" 
+                placeholder="Quantidade" 
+                value={quantidade} 
+                onChange={(e) => setQuantidade(parseInt( e.target.value))} />
+
+                <input 
+                className="input--metade" 
+                type="text" 
+                id="descricao" 
+                placeholder="Descrição" 
+                value={descricao} 
+                onChange={(e) => setDescricao( e.target.value)} />
                 </div>
-
-                { editar && <button 
-                type="submit"
-                className="btn--cadastro"
-                onClick={() => {
-                    setEditar(false);
-                    limparFormulario()
-                }}
-                >Cancelar
-                </button>}
-                 {" "}
-
-                <button 
-                type="submit" 
-                className="btn--cadastro">
-               Adicionar Produto
-                </button>
-
+           
+                {editar &&  
+                 <button 
+                    type="button"
+                    className="btn--cadastro"
+                    onClick={() => {
+                    setEditar(false); //faz esconder o botao editar
+                    setId(0)
+                    limparFormulario(); //reseta os states dos inputs   
+                 }}
+                   
+                  >
+                    Cancelar 
+                   </button>}
+                {" "}
+                <button type="submit" className="btn--cadastro">
+                    Adicionar Produto
+                 </button>
             </form>
 
-
+        
             <section className="produtos">
                 {arrProdutos.map((prod) => (
                     <div key={prod.id} className="produto">
-                        <h2 className="produtos__titulo">{prod.nome}</h2>
-                        <p className="produtos__descricao">Preço: R$ {prod.preco.toFixed(2)}</p>
-                        <p className="produtos__descricao">Descrição: {prod.descricao}</p>
-                        <p className="produtos__descricao">Quantidade: {prod.quantidade}</p>
+                        <h2>{prod.nome}</h2>
+                        <p>Preço: R$ {prod.preco.toFixed(2)}</p>
+                        <p>Descrição: {prod.descricao}</p>
+                        <p>Quantidade: {prod.quantidade}</p>
                         <img src={img} alt={prod.nome} />
-                        <a href="" onClick={(e) => {
+                        <a 
+                        href=""
+                         onClick={(e) =>{
                             e.preventDefault()
-                            Deletar(prod.id)
-                        }}>Apagar</a>
-
+                            deletar(prod.id)
+                        }}
+                          >
+                            Apagar
+                            </a>
                         <button className="produtos__btn-comprar">Comprar</button>
-
-                         <a href="" onClick={(e) => {
+                         <a
+                          href="" 
+                          onClick={(e) =>{
                             e.preventDefault()
-                            setEditar(true)
+
+                            setEditar(true);// botao de editar
+                            setId(prod.id)
                             setNome(prod.nome)
                             setDescricao(prod.descricao)
                             setPreco(prod.preco)
                             setQuantidade(prod.quantidade)
-                        }}>Editar</a>
-
+                        }}
+                          >
+                            Editar
+                        </a>
                     </div>
                 ))}
             </section>
